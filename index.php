a6c3b1a128856fb0f365417a55ad759bfb965c0d
Belen Ruiz <belen.ruiz@palisis.com>
2025-04-07 09:16:34 +0200

    Solved base paths in index

--- index.php
+++ index.php
@@ -1,16 +1,17 @@
 <?php
 require(__DIR__.'/vendor/autoload.php');
 
-
+use TourCMS\OnBoarding\Config\env;
 $routes = ['/', 'login', 'error', 'channels', 'tours', 'tour'];
 $serverURI = ['', ''];
+$serverName = env::getEnvVariable('SERVER_NAME');
 
 
 $_SERVER['REQUEST_URI'] == '/' ? $serverURI[1] = $_SERVER['REQUEST_URI'] : $serverURI = explode('/', $_SERVER['REQUEST_URI']);
 ;
 
-if (in_array($serverURI[1], $this->routes, true) == false) {
-    header('Location:http://' . $_SERVER['SERVER_NAME'] . '/error');
+if (in_array($serverURI[1], $routes, true) == false) {
+    header('Location: '.$serverName. '/error');
     exit();
 } else {
     include_once('src/controller/mainController.php');
