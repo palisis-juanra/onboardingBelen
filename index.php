95ecfb3a4bf647adad63c7d04bfea7e55f34ab5b
Belen Ruiz <belen.ruiz@palisis.com>
2025-04-07 09:12:19 +0200

    Solved enrouter changes

--- index.php
+++ index.php
@@ -1,9 +1,22 @@
 <?php
-require('/var/www/html/onboardingBelen/vendor/autoload.php');
+require(__DIR__.'/vendor/autoload.php');
 
-use TourCMS\OnBoarding\Model\enrouter;
-use TourCMS\OnBoarding\Model\tourCMSFactory;
 
-$enrouter = new enrouter();
-$enrouter->enroute();
+$routes = ['/', 'login', 'error', 'channels', 'tours', 'tour'];
+$serverURI = ['', ''];
+
+
+$_SERVER['REQUEST_URI'] == '/' ? $serverURI[1] = $_SERVER['REQUEST_URI'] : $serverURI = explode('/', $_SERVER['REQUEST_URI']);
+;
+
+if (in_array($serverURI[1], $this->routes, true) == false) {
+    header('Location:http://' . $_SERVER['SERVER_NAME'] . '/error');
+    exit();
+} else {
+    include_once('src/controller/mainController.php');
+}
+
+    
+
+
 
