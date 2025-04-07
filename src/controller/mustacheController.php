2b4853880d43c28445a0c5b3351ecb70c646d698
Belen Ruiz <belen.ruiz@palisis.com>
2025-04-07 18:38:36 +0200

    Solved mustache path and templates changes

--- src/controller/mustacheController.php
+++ src/controller/mustacheController.php
@@ -3,6 +3,7 @@ namespace TourCMS\OnBoarding\Controller;
 use Mustache_Engine;
 use Mustache_Autoloader;
 use Mustache_Loader_FilesystemLoader;
+use TourCMS\OnBoarding\Config\env;
 class mustacheController
 {
     public $template;
@@ -18,8 +19,8 @@ class mustacheController
     {
         Mustache_Autoloader::register();
         $mustache = new Mustache_Engine(array(
-            'loader' => new Mustache_Loader_FilesystemLoader('/var/www/html/onboardingBelen/src/view/templates'),
-            'partials_loader' => new Mustache_Loader_FilesystemLoader('/var/www/html/onboardingBelen/src/view/templates/partials'),
+            'loader' => new Mustache_Loader_FilesystemLoader(env::getEnvVariable("BASE_PATH").'src/view/templates'),
+            'partials_loader' => new Mustache_Loader_FilesystemLoader(env::getEnvVariable("BASE_PATH").'src/view/templates/partials'),
         ));
 
         if ($this->type == 'template') {
